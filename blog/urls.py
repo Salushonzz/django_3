0676d5a56af300f42d3b29a8b8a2a2b0b560055b
shybu8 <bushink04@gmail.com>
2023-04-17 08:22:37 +0300

    'logo.jpg not found' fixed

--- blog/urls.py
+++ blog/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from blog import settings
 import articles.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', articles.views.archive),
     path('articles/', articles.views.archive),
-]
+] + static(settings.STATIC_URL)
